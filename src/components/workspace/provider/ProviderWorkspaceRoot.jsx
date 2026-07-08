b6e315c5c3c7956ef5240862c2973178f449d509
optilun <contact@optilun.com>
2026-07-09 02:29:20 +0300

    Pass navigation helpers to provider locations module

--- src/components/workspace/provider/ProviderWorkspaceRoot.jsx
+++ src/components/workspace/provider/ProviderWorkspaceRoot.jsx
@@ -63,7 +63,7 @@ export default function ProviderWorkspaceRoot({ user, workspace, onLogout, onRef
         <>
           {section === "overview" && <ProviderOverview overview={overview} onNavigate={navigate} />}
           {section === "profile" && <ProviderProfilePublic locationId={selectedLocationId} overview={overview} workspace={workspace} onNavigate={navigate} onSelectLocation={setSelectedLocationId} onRefresh={() => loadOverview(selectedLocationId)} />}
-          {section === "locations" && <ProviderLocations workspace={workspace} selectedLocationId={selectedLocationId} onSelect={setSelectedLocationId} />}
+          {section === "locations" && <ProviderLocations workspace={workspace} selectedLocationId={selectedLocationId} onSelect={setSelectedLocationId} onNavigate={navigate} onRefresh={() => loadOverview(selectedLocationId)} />}
           {section === "services" && <ProviderServices locationId={selectedLocationId} overview={overview} onRefresh={() => loadOverview(selectedLocationId)} />}
           {section === "team" && <ProviderTeam locationId={selectedLocationId} />}
           {section === "hours" && <ProviderHours locationId={selectedLocationId} onRefresh={() => loadOverview(selectedLocationId)} />}
