1ec6a3b0555b0c22b45178a803aa9ea722ac5d64
optilun <contact@optilun.com>
2026-07-11 00:21:12 +0300

    Add scoped Services workspace polish wrapper

--- src/components/workspace/provider/ProviderServices.jsx
+++ src/components/workspace/provider/ProviderServices.jsx
@@ -1,1 +1,11 @@
-export { default } from "./ProviderServicesWorkspace";
+import React from "react";
+import ProviderServicesWorkspace from "./ProviderServicesWorkspace";
+import "./ProviderServicesPolish.css";
+
+export default function ProviderServices(props) {
+  return (
+    <div className="provider-services-polish">
+      <ProviderServicesWorkspace {...props} />
+    </div>
+  );
+}
