949afafc85cb2bb25c45d76582c991907c4d6822
optilun <contact@optilun.com>
2026-07-10 21:16:00 +0300

    Fix prerequisite equipment fallback and simplify engine

--- shared/servicePrerequisiteEngine.js
+++ shared/servicePrerequisiteEngine.js
@@ -1,16 +1,13 @@
-import {
-  getCanonicalServiceDefinition,
-  normalizeServiceKey,
-} from './canonicalServiceRegistry.js';
+import { normalizeServiceKey } from './canonicalServiceRegistry.js';
 
-const PROFESSIONAL_TYPE_ALIASES = {
+const PROFESSIONAL_ALIASES = {
   medic_oftalmolog: 'ophthalmologist',
   ophthalmologist: 'ophthalmologist',
   optometrist: 'optometrist',
   optician: 'optician',
 };
 
-const EQUIPMENT_TYPE_ALIASES = {
+const EQUIPMENT_ALIASES = {
   ocular_ultrasound: 'ophthalmic_ultrasound',
   ultrasound: 'ophthalmic_ultrasound',
   corneal_topography: 'corneal_topographer',
@@ -18,33 +15,17 @@ const EQUIPMENT_TYPE_ALIASES = {
 };
 
 const GROUP_EQUIPMENT_DEFAULTS = {
-  optometry: {
-    mode: 'any',
-    types: ['visual_acuity_chart', 'phoropter', 'autorefractometer', 'slit_lamp'],
-  },
-  ophthalmology_consults: {
-    mode: 'any',
-    types: ['slit_lamp', 'visual_acuity_chart', 'tonometer'],
-  },
-  children_and_prevention: {
-    mode: 'any',
-    types: ['visual_acuity_chart', 'phoropter', 'autorefractometer', 'slit_lamp'],
-  },
-  technical_activities: {
-    mode: 'any',
-    types: ['tracer', 'blocker', 'edger', 'groover', 'drill', 'polisher', 'ultrasonic_cleaner'],
-  },
+  optometry: { mode: 'any', types: ['visual_acuity_chart', 'phoropter', 'autorefractometer', 'slit_lamp'] },
+  ophthalmology_consults: { mode: 'any', types: ['slit_lamp', 'visual_acuity_chart', 'tonometer'] },
+  children_and_prevention: { mode: 'any', types: ['visual_acuity_chart', 'phoropter', 'autorefractometer', 'slit_lamp'] },
+  technical_activities: { mode: 'any', types: ['tracer', 'blocker', 'edger', 'groover', 'drill', 'polisher', 'ultrasonic_cleaner'] },
 };
 
 const SERVICE_POLICY_OVERRIDES = {
-  pd_measurement: {
-    equipment_mode: 'any',
-    equipment_types: ['pupillometer', 'digital_centering_system'],
-  },
-  digital_centering: {
-    equipment_mode: 'any',
-    equipment_types: ['digital_centering_system'],
-  },
+  pd_measurement: { equipment_mode: 'any', equipment_types: ['pupillometer', 'digital_centering_system'] },
+  digital_centering: { equipment_mode: 'any', equipment_types: ['digital_centering_system'] },
+
+  optometry_consultation: { equipment_mode: 'any', equipment_types: ['visual_acuity_chart', 'phoropter', 'autorefractometer', 'slit_lamp'] },
   visual_acuity_test: { equipment_mode: 'any', equipment_types: ['visual_acuity_chart'] },
   refraction: { equipment_mode: 'any', equipment_types: ['phoropter', 'autorefractometer'] },
   autorefractometry: { equipment_mode: 'any', equipment_types: ['autorefractometer'] },
@@ -53,43 +34,19 @@ const SERVICE_POLICY_OVERRIDES = {
   color_vision_test: { requires_equipment: false, equipment_types: [] },
   occupational_vision: { equipment_mode: 'any', equipment_types: ['visual_acuity_chart'] },
 
-  contact_lens_consultation: {
-    equipment_mode: 'any',
-    equipment_types: ['slit_lamp', 'contact_lens_trial_set'],
-  },
-  contact_lens_fitting: {
-    equipment_mode: 'all',
-    equipment_types: ['slit_lamp', 'contact_lens_trial_set'],
-  },
-  contact_lens_trial: {
-    equipment_mode: 'any',
-    equipment_types: ['slit_lamp', 'contact_lens_trial_set'],
-  },
+  contact_lens_consultation: { equipment_mode: 'any', equipment_types: ['slit_lamp', 'contact_lens_trial_set'] },
+  contact_lens_fitting: { equipment_mode: 'all', equipment_types: ['slit_lamp', 'contact_lens_trial_set'] },
+  contact_lens_trial: { equipment_mode: 'any', equipment_types: ['slit_lamp', 'contact_lens_trial_set'] },
   contact_lens_followup: { equipment_mode: 'any', equipment_types: ['slit_lamp'] },
 
-  ophthalmology_consultation: {
-    equipment_mode: 'any',
-    equipment_types: ['slit_lamp', 'visual_acuity_chart'],
-  },
-  complete_eye_exam: {
-    equipment_mode: 'all',
-    equipment_types: ['slit_lamp', 'visual_acuity_chart'],
-  },
-  prescription_check: {
-    equipment_mode: 'any',
-    equipment_types: ['visual_acuity_chart', 'phoropter', 'autorefractometer'],
-  },
+  ophthalmology_consultation: { equipment_mode: 'any', equipment_types: ['slit_lamp', 'visual_acuity_chart'] },
+  complete_eye_exam: { equipment_mode: 'all', equipment_types: ['slit_lamp', 'visual_acuity_chart'] },
+  prescription_check: { equipment_mode: 'any', equipment_types: ['visual_acuity_chart', 'phoropter', 'autorefractometer'] },
   eye_pressure_check: { equipment_mode: 'any', equipment_types: ['tonometer'] },
   fundus_exam: { equipment_mode: 'any', equipment_types: ['slit_lamp', 'fundus_camera'] },
   anterior_segment_exam: { equipment_mode: 'any', equipment_types: ['slit_lamp'] },
-  followup_consultation: {
-    equipment_mode: 'any',
-    equipment_types: ['slit_lamp', 'visual_acuity_chart'],
-  },
-  second_opinion: {
-    equipment_mode: 'any',
-    equipment_types: ['slit_lamp', 'visual_acuity_chart'],
-  },
+  followup_consultation: { equipment_mode: 'any', equipment_types: ['slit_lamp', 'visual_acuity_chart'] },
+  second_opinion: { equipment_mode: 'any', equipment_types: ['slit_lamp', 'visual_acuity_chart'] },
 
   oct: { equipment_mode: 'any', equipment_types: ['oct'] },
   visual_field_analyzer: { equipment_mode: 'any', equipment_types: ['visual_field_analyzer'] },
@@ -104,131 +61,38 @@ const SERVICE_POLICY_OVERRIDES = {
   specular_microscopy: { equipment_mode: 'any', equipment_types: ['specular_microscope'] },
   angiography: { equipment_mode: 'any', equipment_types: ['retinal_angiography_system'] },
 
-  children_eye_exam: {
-    professional_types: ['ophthalmologist'],
-    equipment_mode: 'any',
-    equipment_types: ['slit_lamp', 'visual_acuity_chart'],
-  },
-  pediatric_refraction: {
-    professional_types: ['optometrist', 'ophthalmologist'],
-    equipment_mode: 'any',
-    equipment_types: ['visual_acuity_chart', 'phoropter', 'autorefractometer'],
-  },
-  amblyopia_screening: {
-    professional_types: ['ophthalmologist'],
-    equipment_mode: 'any',
-    equipment_types: ['visual_acuity_chart'],
-  },
-  strabismus_screening: {
-    professional_types: ['ophthalmologist'],
-    equipment_mode: 'any',
-    equipment_types: ['visual_acuity_chart'],
-  },
-  school_screening: {
-    professional_types: ['optometrist', 'ophthalmologist'],
-    equipment_mode: 'any',
-    equipment_types: ['visual_acuity_chart'],
-  },
-  myopia_control_children: {
-    professional_types: ['optometrist', 'ophthalmologist'],
-    equipment_mode: 'any',
-    equipment_types: ['visual_acuity_chart', 'phoropter', 'autorefractometer'],
-  },
-  vision_therapy: {
-    professional_types: ['optometrist', 'ophthalmologist'],
-    requires_equipment: false,
-    equipment_types: [],
-  },
+  children_eye_exam: { professional_types: ['ophthalmologist'], equipment_mode: 'any', equipment_types: ['slit_lamp', 'visual_acuity_chart'] },
+  pediatric_refraction: { professional_types: ['optometrist', 'ophthalmologist'], equipment_mode: 'any', equipment_types: ['visual_acuity_chart', 'phoropter', 'autorefractometer'] },
+  amblyopia_screening: { professional_types: ['ophthalmologist'], equipment_mode: 'any', equipment_types: ['visual_acuity_chart'] },
+  strabismus_screening: { professional_types: ['ophthalmologist'], equipment_mode: 'any', equipment_types: ['visual_acuity_chart'] },
+  school_screening: { professional_types: ['optometrist', 'ophthalmologist'], equipment_mode: 'any', equipment_types: ['visual_acuity_chart'] },
+  myopia_control_children: { professional_types: ['optometrist', 'ophthalmologist'], equipment_mode: 'any', equipment_types: ['visual_acuity_chart', 'phoropter', 'autorefractometer'] },
+  vision_therapy: { professional_types: ['optometrist', 'ophthalmologist'], requires_equipment: false, equipment_types: [] },
 
-  cataract_surgery: {
-    equipment_mode: 'all',
-    equipment_types: ['operating_microscope', 'phacoemulsification_system'],
-    infrastructure_types: ['surgical_infrastructure'],
-  },
-  refractive_surgery: {
-    equipment_mode: 'any',
-    equipment_types: ['excimer_laser', 'femtosecond_laser'],
-    infrastructure_types: ['surgical_infrastructure'],
-  },
-  laser_procedures: {
-    equipment_mode: 'any',
-    equipment_types: ['yag_laser', 'retinal_laser', 'excimer_laser', 'femtosecond_laser'],
-    infrastructure_types: ['clinical_procedure_infrastructure'],
-  },
-  yag_laser: {
-    equipment_mode: 'any',
-    equipment_types: ['yag_laser'],
-    infrastructure_types: ['clinical_procedure_infrastructure'],
-  },
-  retinal_laser: {
-    equipment_mode: 'any',
-    equipment_types: ['retinal_laser'],
-    infrastructure_types: ['clinical_procedure_infrastructure'],
-  },
-  intravitreal_injections: {
-    equipment_mode: 'any',
-    equipment_types: ['intravitreal_injection_setup'],
-    infrastructure_types: ['clinical_procedure_infrastructure'],
-  },
-  eyelid_surgery: {
-    equipment_mode: 'any',
-    equipment_types: ['operating_microscope', 'minor_procedure_set'],
-    infrastructure_types: ['surgical_infrastructure'],
-  },
-  chalazion_treatment: {
-    equipment_mode: 'any',
-    equipment_types: ['minor_procedure_set'],
-    infrastructure_types: ['clinical_procedure_infrastructure'],
-  },
-  minor_eye_procedures: {
-    equipment_mode: 'any',
-    equipment_types: ['minor_procedure_set'],
-    infrastructure_types: ['clinical_procedure_infrastructure'],
-  },
+  cataract_surgery: { equipment_mode: 'all', equipment_types: ['operating_microscope', 'phacoemulsification_system'], infrastructure_types: ['surgical_infrastructure'] },
+  refractive_surgery: { equipment_mode: 'any', equipment_types: ['excimer_laser', 'femtosecond_laser'], infrastructure_types: ['surgical_infrastructure'] },
+  laser_procedures: { equipment_mode: 'any', equipment_types: ['yag_laser', 'retinal_laser', 'excimer_laser', 'femtosecond_laser'], infrastructure_types: ['clinical_procedure_infrastructure'] },
+  yag_laser: { equipment_mode: 'any', equipment_types: ['yag_laser'], infrastructure_types: ['clinical_procedure_infrastructure'] },
+  retinal_laser: { equipment_mode: 'any', equipment_types: ['retinal_laser'], infrastructure_types: ['clinical_procedure_infrastructure'] },
+  intravitreal_injections: { equipment_mode: 'any', equipment_types: ['intravitreal_injection_setup'], infrastructure_types: ['clinical_procedure_infrastructure'] },
+  eyelid_surgery: { equipment_mode: 'any', equipment_types: ['operating_microscope', 'minor_procedure_set'], infrastructure_types: ['surgical_infrastructure'] },
+  chalazion_treatment: { equipment_mode: 'any', equipment_types: ['minor_procedure_set'], infrastructure_types: ['clinical_procedure_infrastructure'] },
+  minor_eye_procedures: { equipment_mode: 'any', equipment_types: ['minor_procedure_set'], infrastructure_types: ['clinical_procedure_infrastructure'] },
 
   eyeglasses_adjustment: { requires_equipment: false, equipment_types: [] },
-  eyeglasses_repair: {
-    equipment_mode: 'any',
-    equipment_types: ['drill', 'groover', 'polisher'],
-    infrastructure_types: ['optical_workshop_infrastructure'],
-  },
-  lens_fitting: {
-    equipment_mode: 'any',
-    equipment_types: ['tracer', 'blocker', 'edger'],
-    infrastructure_types: ['optical_workshop_infrastructure'],
-  },
-  frame_repair: {
-    equipment_mode: 'any',
-    equipment_types: ['drill', 'groover', 'polisher'],
-    infrastructure_types: ['optical_workshop_infrastructure'],
-  },
+  eyeglasses_repair: { equipment_mode: 'any', equipment_types: ['drill', 'groover', 'polisher'], infrastructure_types: ['optical_workshop_infrastructure'] },
+  lens_fitting: { equipment_mode: 'any', equipment_types: ['tracer', 'blocker', 'edger'], infrastructure_types: ['optical_workshop_infrastructure'] },
+  frame_repair: { equipment_mode: 'any', equipment_types: ['drill', 'groover', 'polisher'], infrastructure_types: ['optical_workshop_infrastructure'] },
   screw_replacement: { requires_equipment: false, equipment_types: [] },
-  lens_replacement: {
-    equipment_mode: 'any',
-    equipment_types: ['tracer', 'blocker', 'edger'],
-    infrastructure_types: ['optical_workshop_infrastructure'],
-  },
+  lens_replacement: { equipment_mode: 'any', equipment_types: ['tracer', 'blocker', 'edger'], infrastructure_types: ['optical_workshop_infrastructure'] },
   frame_cleaning: { equipment_mode: 'any', equipment_types: ['ultrasonic_cleaner'] },
   workshop_orders: { requires_equipment: false, equipment_types: [] },
 };
 
 const INFRASTRUCTURE_ALIASES = {
-  optical_workshop_infrastructure: [
-    'laborator_optic_propriu',
-    'atelier_service_propriu',
-    'reparatii_pe_loc',
-    'montaj_lentile_in_locatie',
-  ],
-  clinical_procedure_infrastructure: [
-    'clinical_procedure_room',
-    'sterile_procedure_room',
-    'day_procedure_unit',
-  ],
-  surgical_infrastructure: [
-    'operating_room',
-    'day_surgery_unit',
-    'surgical_unit',
-  ],
+  optical_workshop_infrastructure: ['laborator_optic_propriu', 'atelier_service_propriu', 'reparatii_pe_loc', 'montaj_lentile_in_locatie'],
+  clinical_procedure_infrastructure: ['clinical_procedure_room', 'sterile_procedure_room', 'day_procedure_unit'],
+  surgical_infrastructure: ['operating_room', 'day_surgery_unit', 'surgical_unit'],
 };
 
 function clean(value) {
@@ -237,16 +101,16 @@ function clean(value) {
 
 function normalizeProfessionalType(value) {
   const raw = clean(value);
-  return PROFESSIONAL_TYPE_ALIASES[raw] || raw;
+  return PROFESSIONAL_ALIASES[raw] || raw;
 }
 
 function normalizeEquipmentType(value) {
   const raw = clean(value);
-  return EQUIPMENT_TYPE_ALIASES[raw] || raw;
+  return EQUIPMENT_ALIASES[raw] || raw;
 }
 
 function activeRow(row) {
-  return row && row.is_active !== false && row.active !== false && row.active_status !== 'inactiv';
+  return Boolean(row) && row.is_active !== false && row.active !== false && row.active_status !== 'inactiv';
 }
 
 function profileMap(profiles) {
@@ -267,10 +131,8 @@ function verifiedProfessionalTypes(assignments, profiles) {
       || profile?.confirmation_level === 'vezunde_verified'
       || profile?.verified === true;
     if (!verified) continue;
-    const professionalType = normalizeProfessionalType(
-      assignment.professional_type || profile?.professional_type || profile?.role,
-    );
-    if (professionalType) result.add(professionalType);
+    const type = normalizeProfessionalType(assignment.professional_type || profile?.professional_type || profile?.role);
+    if (type) result.add(type);
   }
   return result;
 }
@@ -287,10 +149,8 @@ function verifiedEquipmentTypes(equipment, medical) {
       ? confirmation === 'vezunde_verified'
       : ['provider_confirmed', 'vezunde_verified'].includes(confirmation);
     if (!evidenceApproved || !confirmationAccepted) continue;
-    const equipmentType = normalizeEquipmentType(
-      item.equipment_category_key || item.equipment_key || item.key,
-    );
-    if (equipmentType) result.add(equipmentType);
+    const type = normalizeEquipmentType(item.equipment_category_key || item.equipment_key || item.key);
+    if (type) result.add(type);
   }
   return result;
 }
@@ -299,8 +159,8 @@ function activeFacilityTypes(facilities) {
   const result = new Set();
   for (const facility of facilities || []) {
     if (!activeRow(facility)) continue;
-    const facilityType = clean(facility.facility_key || facility.key);
-    if (facilityType) result.add(facilityType);
+    const type = clean(facility.facility_key || facility.key);
+    if (type) result.add(type);
   }
   return result;
 }
@@ -323,26 +183,26 @@ function infrastructureSatisfied(requirement, facilities, location) {
 export function getServicePrerequisiteDefinition(rawKey) {
   const normalized = normalizeServiceKey(rawKey);
   if (!normalized.canonicalKey || !normalized.definition) return null;
+
   const base = normalized.definition;
   const override = SERVICE_POLICY_OVERRIDES[normalized.canonicalKey] || {};
   const groupEquipment = GROUP_EQUIPMENT_DEFAULTS[base.group] || null;
-  const requiredEquipmentTypes = override.equipment_types
-    || base.required_equipment_types
-    || groupEquipment?.types
-    || [];
-  const equipmentMode = override.equipment_mode || groupEquipment?.mode || 'all';
-  const requiredInfrastructureTypes = override.infrastructure_types
-    || (base.requires_infrastructure ? ['clinical_procedure_infrastructure'] : []);
+  const baseEquipment = Array.isArray(base.required_equipment_types) && base.required_equipment_types.length > 0
+    ? base.required_equipment_types
+    : null;
+  const requiredEquipmentTypes = override.equipment_types !== undefined
+    ? override.equipment_types
+    : (baseEquipment || groupEquipment?.types || []);
+  const requiredInfrastructureTypes = override.infrastructure_types !== undefined
+    ? override.infrastructure_types
+    : (base.requires_infrastructure ? ['clinical_procedure_infrastructure'] : []);
+
   return {
     ...base,
-    required_professional_types: [
-      ...(override.professional_types || base.required_professional_types || []),
-    ],
-    requires_equipment: override.requires_equipment === undefined
-      ? base.requires_equipment
-      : override.requires_equipment,
+    required_professional_types: [...(override.professional_types || base.required_professional_types || [])],
+    requires_equipment: override.requires_equipment === undefined ? base.requires_equipment : override.requires_equipment,
     required_equipment_types: [...requiredEquipmentTypes],
-    equipment_requirement_mode: equipmentMode,
+    equipment_requirement_mode: override.equipment_mode || groupEquipment?.mode || 'all',
     requires_infrastructure: requiredInfrastructureTypes.length > 0 || base.requires_infrastructure,
     required_infrastructure_types: [...requiredInfrastructureTypes],
   };
@@ -358,11 +218,7 @@ export function evaluateServicePrerequisites(rawKey, context = {}) {
       status: 'unknown_service',
       blockers: [{ code: 'unknown_service', message: 'Serviciul nu exista in registrul canonic.' }],
       definition: null,
-      evidence: {
-        verified_professional_types: [],
-        verified_equipment_types: [],
-        active_facility_types: [],
-      },
+      evidence: { verified_professional_types: [], verified_equipment_types: [], active_facility_types: [] },
     };
   }
 
@@ -410,9 +266,7 @@ export function evaluateServicePrerequisites(rawKey, context = {}) {
       });
     } else {
       const checks = required.map((type) => equipmentTypes.has(normalizeEquipmentType(type)));
-      const matched = definition.equipment_requirement_mode === 'any'
-        ? checks.some(Boolean)
-        : checks.every(Boolean);
+      const matched = definition.equipment_requirement_mode === 'any' ? checks.some(Boolean) : checks.every(Boolean);
       if (!matched) {
         blockers.push({
           code: 'verified_equipment_missing',
@@ -470,6 +324,7 @@ export function servicePrerequisiteStatusLabel(status) {
   const labels = {
     available: 'Disponibil',
     ready_for_review: 'Pregatit pentru verificare',
+    requires_verified_location: 'Necesita locatie verificata',
     requires_verified_specialist: 'Necesita specialist verificat',
     requires_equipment: 'Necesita echipament verificat',
     requires_infrastructure: 'Necesita infrastructura verificata',
