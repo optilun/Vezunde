2abb797f7a2d606d224bfcb96c79ca6880510aea
optilun <contact@optilun.com>
2026-07-11 11:24:44 +0300

    Reset Services icons to clean Lucide treatment
    * Remove custom masked service icons
    
    * Delete custom masked service icon styles
    
    * Simplify service category icon treatment

--- src/components/workspace/provider/ProviderServices.jsx
+++ src/components/workspace/provider/ProviderServices.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ProviderServicesWorkspace from "./ProviderServicesWorkspace";
 import "./ProviderServicesPolish.css";
-import "./ProviderServicesPremiumIcons.css";
 
 export default function ProviderServices(props) {
   return (
