b0890ad0132b51edf1d8e6380aab9648cfca8c00
optilun <contact@optilun.com>
2026-07-13 12:20:40 +0300

    Keep OTP slots compatible with existing typings

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -80,7 +80,12 @@ export default function Register() {
         <div className="flex justify-center mb-6">
           <InputOTP maxLength={6} value={otpCode} onChange={setOtpCode} autoFocus autoComplete="one-time-code">
             <InputOTPGroup>
-              {[0, 1, 2, 3, 4, 5].map((index) => <InputOTPSlot key={index} index={index} />)}
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+              <InputOTPSlot index={3} />
+              <InputOTPSlot index={4} />
+              <InputOTPSlot index={5} />
             </InputOTPGroup>
           </InputOTP>
         </div>
