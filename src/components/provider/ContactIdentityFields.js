1286a95791af00b9c6c93394e1364bbe47640133
optilun <contact@optilun.com>
2026-07-13 11:52:58 +0300

    Define requested provider roles from claimant relationships

---
+++ src/components/provider/ContactIdentityFields.js
@@ -0,0 +1,23 @@
+export const CLAIMANT_RELATIONSHIPS = {
+  owner: "Proprietar sau reprezentant legal",
+  organization_representative: "Reprezentant autorizat al organizatiei",
+  location_manager: "Manager al locatiei",
+  authorized_staff: "Angajat cu acordul organizatiei",
+};
+
+export const REQUESTED_ROLE_BY_RELATIONSHIP = {
+  owner: "organization_owner",
+  organization_representative: "organization_owner",
+  location_manager: "location_manager",
+  authorized_staff: "location_staff",
+};
+
+export const REQUESTED_ROLE_LABELS = {
+  organization_owner: "Owner organizatie",
+  location_manager: "Manager locatie",
+  location_staff: "Membru locatie",
+};
+
+export function requestedRoleForRelationship(relationship) {
+  return REQUESTED_ROLE_BY_RELATIONSHIP[relationship] || "location_staff";
+}
