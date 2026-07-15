09f95cf36868a08ebec175cfbdcc0f74395cb1c7
optilun <contact@optilun.com>
2026-07-16 00:34:37 +0300

    Use bundled trust dependency in legacy matching

--- base44/functions/matchProviders/entry.ts
+++ base44/functions/matchProviders/entry.ts
@@ -5,7 +5,7 @@ import {
   isServicePubliclyEligible,
   normalizeServiceKey,
 } from './sharedDependencies.js';
-import { getPublicLocationDisclosure } from '../../../shared/providerPublicTrust.js';
+import { getPublicLocationDisclosure } from './providerPublicTrust.js';
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.31';
 
 // Safety rules remain disabled until reviewed by a qualified ophthalmologist.
