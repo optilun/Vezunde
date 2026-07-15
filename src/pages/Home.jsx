dcb80255d017a404722468c2a0512546e6873000
optilun <contact@optilun.com>
2026-07-15 12:04:12 +0300

    Replace homepage marquee with categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "@/components/home/Hero";
-import MarqueeStrip from "@/components/home/MarqueeStrip";
+import CategoryShowcase from "@/components/home/CategoryShowcase";
 import SituationExplainer from "@/components/home/SituationExplainer";
 import ServicesEditorial from "@/components/home/ServicesEditorial";
 import ProvidersShowcase from "@/components/home/ProvidersShowcase";
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="pb-16 overflow-x-clip">
       <Hero />
-      <MarqueeStrip />
+      <CategoryShowcase />
       <SituationExplainer />
       <ServicesEditorial />
       <ProvidersShowcase />
