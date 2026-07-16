55dd8faed0c538f3f08c55a8490678efd3b095cf
optilun <contact@optilun.com>
2026-07-16 11:25:53 +0300

    Add canonical VIASEE legal identity

---
+++ src/lib/legal.js
@@ -0,0 +1,11 @@
+export const VIASEE_COMPANY = {
+  legalName: "Lunera Optic SRL",
+  brandName: "VIASEE",
+  taxId: "53362575",
+  registrationNumber: "J2026003935004",
+  registeredOffice: "România",
+  contactEmail: "contact@viasee.ro",
+};
+
+export const LEGAL_LAST_UPDATED = "16 iulie 2026";
+
