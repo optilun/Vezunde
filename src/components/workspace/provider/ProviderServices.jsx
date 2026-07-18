3c3084d3cefa748d73a3b4381b5f5e43b01a13f8
optilun <contact@optilun.com>
2026-07-19 01:00:08 +0300

    Load services action bar refinement

--- src/components/workspace/provider/ProviderServices.jsx
+++ src/components/workspace/provider/ProviderServices.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ProviderServicesThreeColumn from "./ProviderServicesThreeColumn";
 import "./ProviderServicesThreeColumn.css";
 import "./ProviderServicesRefinement.css";
+import "./ProviderServicesActionBar.css";
 
 export default function ProviderServices(props) {
   return <ProviderServicesThreeColumn {...props} />;
