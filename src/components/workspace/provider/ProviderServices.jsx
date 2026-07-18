c90e6f6879d4a81a7b87aea5f94e7f11c5eebaa1
optilun <contact@optilun.com>
2026-07-18 04:29:02 +0300

    Use progressive services layout

--- src/components/workspace/provider/ProviderServices.jsx
+++ src/components/workspace/provider/ProviderServices.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import ProviderServicesWorkspaceRuntime from "./ProviderServicesWorkspaceRuntime";
+import ProviderServicesProgressive from "./ProviderServicesProgressive";
 import "./ProviderServicesClean.css";
 
 export default function ProviderServices(props) {
-  return (
-    <div className="provider-services-compact">
-      <div className="provider-services-polish">
-        <ProviderServicesWorkspaceRuntime {...props} />
-      </div>
-    </div>
-  );
+  return <ProviderServicesProgressive {...props} />;
 }
