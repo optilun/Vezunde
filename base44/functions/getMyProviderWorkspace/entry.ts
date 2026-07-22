35edb2ce150ac8ff1bd9031d3d33c02010f995dc
optilun <contact@optilun.com>
2026-07-22 12:06:57 +0300

    Add getMyProviderWorkspace runtime deploy revision

--- base44/functions/getMyProviderWorkspace/entry.ts
+++ base44/functions/getMyProviderWorkspace/entry.ts
@@ -1,3 +1,6 @@
 import { handleProviderWorkspaceRequest } from './router.ts';
 
+const FUNCTION_DEPLOY_REVISION = 'viasee-runtime-resync-2026-07-22-getMyProviderWorkspace-1';
+console.info(`[VIASEE] getMyProviderWorkspace ${FUNCTION_DEPLOY_REVISION}`);
+
 Deno.serve(handleProviderWorkspaceRequest);
