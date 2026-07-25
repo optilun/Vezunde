52753b6489e9cdb4ebe6aeedf7119eefa518c852
optilun <contact@optilun.com>
2026-07-25 13:54:11 +0300

    Bridge legacy backend safety imports to v1.2

---
+++ base44/shared/patientSafety.js
@@ -0,0 +1,20 @@
+import {
+  PATIENT_EYE_SAFETY_POLICY_VERSION,
+  PATIENT_SAFETY_FLAG_PRESENTATION,
+  advisorySafetyFlagsFromText,
+  assessPatientEyeSafety,
+  deterministicSafetyFlagsFromText,
+  guidedSafetyFlagsFromAnswers,
+} from './patientEyeSafetyPolicy.js';
+
+export const PATIENT_SAFETY_ASSESSMENT_VERSION = PATIENT_EYE_SAFETY_POLICY_VERSION;
+export { PATIENT_SAFETY_FLAG_PRESENTATION };
+export {
+  advisorySafetyFlagsFromText,
+  deterministicSafetyFlagsFromText,
+  guidedSafetyFlagsFromAnswers,
+};
+
+export function buildPatientSafetyAssessment(options = {}) {
+  return assessPatientEyeSafety(options);
+}
