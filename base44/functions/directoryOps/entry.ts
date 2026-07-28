ed5d72e80bc74ae2b89efb8b8e6826f4630e721b
optilun <contact@optilun.com>
2026-07-28 22:54:11 +0300

    Force directoryOps redeploy for location-first imports

--- base44/functions/directoryOps/entry.ts
+++ base44/functions/directoryOps/entry.ts
@@ -1,6 +1,6 @@
 import { handleDirectoryRequest } from './router.ts';
 
-const FUNCTION_DEPLOY_REVISION = 'viasee-runtime-resync-2026-07-22-directoryOps-3';
+const FUNCTION_DEPLOY_REVISION = 'viasee-runtime-resync-2026-07-28-directory-location-first-1';
 console.info(`[VIASEE] directoryOps ${FUNCTION_DEPLOY_REVISION}`);
 
 Deno.serve(handleDirectoryRequest);
